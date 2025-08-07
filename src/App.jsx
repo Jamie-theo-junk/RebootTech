d72043552fcc7e09cf76738f9c39c9fcb50ff417
Jamie Theo Junk <jamingwithjamie@gmail.com>
2025-08-07 20:14:24 +0200

    improved homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from './pages/home/home'
 import Navbar from "./components/navbar/navbar"
+import Footer from './components/Footer/Footer'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes> 
         <Route path="/" element={<Home/>} />
       </Routes>
+      <Footer/>
   </Router>
   )
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,28 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { FaLocationDot } from "react-icons/fa6";
 import { IoPerson } from "react-icons/io5";
 import axios from 'axios';
-import './home.css'
+import './home.css';
+import jamieImage from '../../assets/home_page_images/jamie_image.png';
+import fiverrImage from '../../assets/home_page_images/fiverr_app_logo.png';
+import githubImage from '../../assets/home_page_images/github-app-logo.png';
+import linkinImage from '../../assets/home_page_images/linkin-app-logo.png';
 import AdComponent from "../../components/AdComponent/adComponent"
+import { motion } from "motion/react"
 
 const home = () => {
 
-  const [listings, setListings] = useState([]);
+  const [recentlistings, setRecentListings] = useState([]);
+  const [topSellersList, setTopSellersList] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3000/api/items/recent') 
       .then(response => {
-        setListings(response.data);
+        setRecentListings(response.data);
       })
       .catch(error => {
         console.error('Error fetching listings:', error);
       });
+
+  axios.get('http://localhost:3000/api/users/top-sellers')
+      .then(response => {
+        setTopSellersList(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching top sellers:', error);
+      });
   }, []);
+
+
+
   return (
     <div>
       <section className='new-list-section'>
       <h2>New Listings</h2>
      <div className="cards-container">
-  {listings.map((item) => (
+  {recentlistings.map((item) => (
     <div className="card" key={item._id}>
       <img src={item.imageFile} alt={item.name} className="card-image"/>
       <h3 className="card-title">{item.name}</h3>
@@ -36,14 +53,83 @@ const home = () => {
       </section>
       <section className='top-seller-section'>
       <h2>Top Sellers</h2>
-      {listings.map((item) => (
-    <div className="top-seller-card" key={item._id}>
-      <img src={item.imageFile} alt={item.name} className="top-seller-card-image"/>
-      <img src={item.imageFile} alt={item.name} className="top-seller-card-image"/>
-      <img src={item.imageFile} alt={item.name} className="top-seller-card-image"/>
-      <img src={item.imageFile} alt={item.name} className="top-seller-card-image"/>
+      <div className="card-section">
+   {topSellersList.map((seller, index) => (
+  <div className="top-seller-card" key={seller.name + index}>
+    <h3 className="top-seller-author">{seller.name}</h3>
+    <div className="top-seller-card-images">
+      {seller.images.map((image, i) => (
+        <img
+          key={i}
+          src={image}
+          alt={`${seller.name} item ${i + 1}`}
+          className="top-seller-card-image"
+        />
+      ))}
     </div>
-  ))}
+  </div>
+))}
+
+     </div>
+      </section>
+      <section className="about-me-section">
+      <div className="image-half">
+      <img src={jamieImage} alt="jamie image" style={{ width: '300px', height: '300px' }} />
+      </div>
+      <div className='text-half'>
+      <h3>Hi :)</h3>
+      <p>I'm Jamie Theo Junk, an aspiring programmer. I created this application on my own because I noticed there's no reliable place to buy and sell tech. While I know this platform isn't perfect yet, I strive to make it a one-stop shop for all things tech. As technology continues to advance, the need to buy brand-new devices will decrease—previous generations will remain just as capable. That’s why platforms like this will be essential.<br></br><br />Hope you enjoy happy scrolling</p>
+      <div className='link-list'>
+      <motion.button
+      whileHover={{ scale: 1.1, rotate: 5 }} 
+      transition={{ type: 'spring', stiffness: 300 }}
+      style={{
+        border: 'none',
+        background: 'transparent',
+        padding: 0,
+        cursor: 'pointer',
+      }}
+    >
+      <img 
+        src={fiverrImage} 
+        alt="Button Image"
+        className='link-btns'
+      />
+    </motion.button>
+          <motion.button
+      whileHover={{ scale: 1.1, rotate: 5 }} 
+      transition={{ type: 'spring', stiffness: 300 }}
+      style={{
+        border: 'none',
+        background: 'transparent',
+        padding: 0,
+        cursor: 'pointer'
+      }}
+    >
+      <img 
+        src={githubImage} 
+        alt="Button Image"
+        className='link-btns'
+      />
+    </motion.button>
+          <motion.button
+      whileHover={{ scale: 1.1, rotate: 5 }} 
+      transition={{ type: 'spring', stiffness: 300 }}
+      style={{
+        border: 'none',
+        background: 'transparent',
+        padding: 0,
+        cursor: 'pointer'
+      }}
+    >
+      <img 
+        src={linkinImage} 
+        alt="Button Image"
+        className='link-btns'
+      />
+    </motion.button>
+      </div>
+      </div>
       </section>
     </div>
   )
